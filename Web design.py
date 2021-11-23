3df4487d5723821a0ccd67a2c3bb008f2e643274
abdulwasih2003 <abdul.wasih03@gmail.com>
2021-11-23 18:28:08 +0530

    python_design

---
+++ Web design.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>My webserver</title>
+</head>
+<body>
+<h1>H.Syed Abdul Wasih</h1>
+<h2>21002291</h2>
+<h3>abdul.wasih03@gmail.com</h3>
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8080)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
